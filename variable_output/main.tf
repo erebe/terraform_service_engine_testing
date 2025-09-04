e27c32ffefee9083d128b1182f92eb5e3cc15134
Fabien FLEUREAU <ffleureau@qovery.com>
2025-09-04 15:56:14 +0200

    fix

--- variable_output/main.tf
+++ variable_output/main.tf
@@ -3,14 +3,18 @@ output "printed_message" {
   description = "Prints the content of the variable"
   value       = var.content
 }
-resource "time_sleep" "staging_delay" {  
-  create_duration = "2m"
+resource "null_resource" "always_recreate" {
   triggers = {
-    always_run = uuid()
+    timestamp = timestamp()
   }
+}
+
+resource "time_sleep" "staging_delay" {
+  create_duration = "2m"
+  
   lifecycle {
     replace_triggered_by = [
-      self.triggers["always_run"]
+      null_resource.always_recreate
     ]
   }
 }
