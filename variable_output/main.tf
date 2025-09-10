0fde58074cd0e301c477d3a6da473caaa7052810
Fabien FLEUREAU <ffleureau@qovery.com>
2025-09-10 15:27:27 +0200

    handle sleep

--- variable_output/main.tf
+++ variable_output/main.tf
@@ -3,18 +3,14 @@ output "printed_message" {
   description = "Prints the content of the variable"
   value       = var.content
 }
-resource "null_resource" "always_recreate" {
+
+resource "null_resource" "staging_delay" {
+  provisioner "local-exec" {
+    command = "sleep ${var.sleep_duration}"
+  }
+
+  # Always recreate (equivalent to original behavior)
   triggers = {
     timestamp = timestamp()
-  }
-}
-
-resource "time_sleep" "staging_delay" {
-  create_duration = "2m"
-  
-  lifecycle {
-    replace_triggered_by = [
-      null_resource.always_recreate
-    ]
   }
 }
--- variable_output/variables.tf
+++ variable_output/variables.tf
@@ -1,4 +1,10 @@
 variable "content" {
-  description = "Content to ouput"
+  description = "Content to output"
   type        = string
+}
+
+variable "sleep_duration" {
+  description = "Duration to sleep in seconds"
+  type        = number
+  default     = 120  # 2 minutes default
 }
