18878075f020bfe02b06dfacef7951b6dcb8167c
Erèbe - Romain Gerard <github@erebe.eu>
2025-09-17 17:38:43 +0200

    Update backend.tf

--- variable_output/backend.tf
+++ variable_output/backend.tf
@@ -1,9 +1,7 @@
 terraform {
-  backend "s3" {
-    bucket = "qovery-erebe-tf-test"
-    key    = "path_to_my_"
-    region = "eu-west-3"
-    use_lockfile = true
+  backend "kubernetes" {
+    secret_suffix  = "ea9d6258-0bbc-47ed-b443-e4aaf06c98ba"
+    namespace      = "za0cd0187-tf"
+    config_path    = "/tmp/terraform_service_engine_testing/variable_output/kubeconfig-be9e22b0-d05a-4330-b5b5-547667d380fd.yaml"
   }
 }
-
